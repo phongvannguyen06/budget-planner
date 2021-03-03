5b457d497bad7104d2be8801b3be18bd7b2d298c
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-03-02 21:26:13 -0500

    add menu component and implement navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VendorComponent } from './vendor/vendor.component';
 import { DealComponent } from './deal/deal.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'vendors', component: VendorComponent },
+  { path: '', component: DealComponent },
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    VendorComponent,
-    DealComponent
-  ],
+  declarations: [AppComponent, VendorComponent, DealComponent, MenuComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
