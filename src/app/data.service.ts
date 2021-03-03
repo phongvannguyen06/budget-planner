d30e10a26b906898babf079619adbe079e749d7f
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-03-02 21:47:36 -0500

    add data service for vendor

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { Vendor } from './model/Vendor';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  vendors: Array<Vendor>;
 
-  constructor() { }
+  getVendors(): Observable<Array<Vendor>> {
+    return of(this.vendors);
+  }
+
+  constructor() {
+
+    // list of initial vendors
+    this.vendors = new Array<Vendor>();
+    const vendor1 = new Vendor();
+    vendor1.id = 1;
+    vendor1.name = 'Microsoft';
+    vendor1.description = 'A large technology company specializes in building windows';
+
+    const vendor2 = new Vendor();
+    vendor2.id = 2;
+    vendor2.name = 'Apple';
+    vendor2.description = 'A very large tech company whose sole revenue is from selling Iphones';
+
+    const vendor3 = new Vendor();
+    vendor3.id = 3
+    vendor3.name = 'Google';
+    vendor3.description = 'A very suspicious tech company that tracks all of your data';
+
+    this.vendors.push(vendor1);
+    this.vendors.push(vendor2);
+    this.vendors.push(vendor3);
+
+
+  }
 }
--- src/app/vendor/vendor.component.ts
+++ src/app/vendor/vendor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Vendor } from '../model/Vendor';
+import {DataService} from '../data.service'
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorComponent implements OnInit {
 
-  constructor() { }
+  vendors: Array<Vendor>
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getVendors().subscribe(next => this.vendors = next);
   }
 
 }
