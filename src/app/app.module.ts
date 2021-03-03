e9a0ba656a11a4bbb63db535a4e3bf7197cc6a77
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-03-02 21:06:54 -0500

    add vendor and deal components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VendorComponent } from './vendor/vendor.component';
+import { DealComponent } from './deal/deal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VendorComponent,
+    DealComponent
   ],
   imports: [
     BrowserModule
